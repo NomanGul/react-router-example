8917bdd64ca5ed0b2a6c61127b928f05ea3e7e08
Noman Gul <nomangul2001@gmail.com>
2018-09-16 01:13:36 +0500

    Importing components and Routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
