b726dcfd698ea6b97166fe220c046765f81fefb1
Noman Gul <nomangul2001@gmail.com>
2018-09-16 01:12:50 +0500

    Contact Complete

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="center blue-text">Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        nesciunt placeat dolor sunt beatae. Rem id officia dicta sit magnam
+        quae. Quisquam quis dicta possimus mollitia, dolore perferendis modi
+        neque.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
