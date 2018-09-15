67cea528e45a5fac008f0d07cb6dc00716e1a12a
Noman Gul <nomangul2001@gmail.com>
2018-09-16 01:12:36 +0500

    Home Complete

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="center blue-text">Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        nesciunt placeat dolor sunt beatae. Rem id officia dicta sit magnam
+        quae. Quisquam quis dicta possimus mollitia, dolore perferendis modi
+        neque.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
